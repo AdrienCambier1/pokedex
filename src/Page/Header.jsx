230cf8f2b0197e7c24e2c01e23c5b706807d6f13
Cambier-Adrien <adrien.cambier04@gmail.com>
2024-12-05 15:37:17 +0100

    {~} Added ScrollToTop function to header

--- src/Page/Header.jsx
+++ src/Page/Header.jsx
@@ -5,23 +5,36 @@ import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons'
 import { LanguageChange, MenuList } from '../Components'
 import { useContext } from 'react'
 import { LanguageContext, ThemeContext } from '../Contexts'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { LightButton } from '../Components/Buttons'
 
 export default function Header() {
   const { Theme, setTheme } = useContext(ThemeContext)
   const { selectedLanguage } = useContext(LanguageContext)
+  const navigate = useNavigate()
 
-  function handleToggleTheme() {
+  const handleToggleTheme = () => {
     const newTheme = Theme === 'dark' ? 'light' : 'dark'
     setTheme(newTheme)
   }
 
+  const ScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  const handleNavigationToHome = () => {
+    navigate('/')
+    ScrollToTop()
+  }
+
   return (
     <header className="w-full border-b border-white dark:border-zinc-700 dark:bg-zinc-900 bg-white shadow-md shadow-gray-300/50 dark:shadow-none h-16 flex fixed z-50 items-center justify-between px-10">
-      <Link to="/" className="h-8 relative">
+      <button onClick={handleNavigationToHome} className="h-8 relative">
         <img className="h-full" src={logo} alt={logo}></img>
-      </Link>
+      </button>
       <div className="flex gap-2">
         <div className="hidden gap-2 sm:flex">
           <LightButton
@@ -29,7 +42,11 @@ export default function Header() {
             icon={faCircleHalfStroke}
             content={traduction[selectedLanguage]['Thème']}
           />
-          <LightButton link="/" icon={faHouse} content={traduction[selectedLanguage]['Accueil']} />
+          <LightButton
+            onClick={handleNavigationToHome}
+            icon={faHouse}
+            content={traduction[selectedLanguage]['Accueil']}
+          />
         </div>
         <LanguageChange />
         <MenuList />
